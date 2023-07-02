cef46fbf24a8c0d42a4da8372a47492dd5e18951
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-07-02 09:37:05 -0300

    style(eslint): adjusted eslint config

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,6 +5,5 @@ import { type theme } from 'styles';
 type ThemeType = typeof theme;
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends ThemeType {}
 }
