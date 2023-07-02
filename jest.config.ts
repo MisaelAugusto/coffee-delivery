2fbbe08bd551c3fc562e25e060f96cab53d0dd5a
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2023-07-02 11:14:49 -0300

    test(tests): adjusted app tests

--- jest.config.ts
+++ jest.config.ts
@@ -10,5 +10,6 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   coveragePathIgnorePatterns: ['src/main.tsx'],
   collectCoverageFrom: ['src/**/*.[jt]s?(x)', '!src/**/__tests__/*'],
-  coverageReporters: ['html', 'text', 'text-summary', 'cobertura']
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+  moduleDirectories: ['src', 'node_modules']
 };
